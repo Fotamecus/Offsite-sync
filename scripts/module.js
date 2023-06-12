4d57a14348c2e2152ca06f19c451ce7f483b9882
Ben Zimny <bjp.zimny@gmail.com>
2023-06-12 16:58:25 +1000

    Test POC

--- scripts/module.js
+++ scripts/module.js
@@ -3,5 +3,7 @@ Hooks.once('init', async function() {
 });
 
 Hooks.once('ready', async function() {
-
+    fetch('http://localhost:3000/foundry')
+        .then(response => response.json())
+        .then(data => console.log(data));
 });
